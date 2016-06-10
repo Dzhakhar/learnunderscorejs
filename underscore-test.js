c9e88a4bf4e5f9c44170be72a4470207cd7921db
dzhakhar <dzhakhar.ukhaev@gmail.com>
2016-06-11 01:50:58 +0600

    index.html added

---
+++ underscore-test.js
@@ -0,0 +1,4 @@
+console.log(_.isEmpty({active: true}));
+console.log(_.flatten([[0,1], [2, 3], [4, 5], ["Hello", "World!"]]));
+console.log(_.range(10));
+var a = _.template('<p><%= text %></p>', {text: 'SitePoint Rocks!'});
